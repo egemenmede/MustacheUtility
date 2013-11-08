4b0e120c181d3e8e060649e4037238ce2451e33e
Egemen MEDE <egemenmede@EGEMEN-MacBook-Pro.local>
2013-11-09 00:14:41 +0200

    Info bilgileri eklendi.

--- mustache.util.js
+++ mustache.util.js
@@ -1,3 +1,13 @@
+/*
+ * Type    : Utility
+ * Name    : MustacheUtility
+ * Author  : DeliPenguen - Egemen MEDE
+ * Date    : 09.11.2013
+ * Time    : 00:10
+ * Web     : http://www.delipenguen.com
+ * Version : 1.0
+ */
+
 mt = function(){
     function mustacheExternalTemplate(templateName, jsonPath, htmlElement){
 		$.ajax({
